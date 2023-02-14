95f64dc9977be1cee7fdc5b5b5e259d5abb6348a
thearithchhie <67414669+thearithchhie@users.noreply.github.com>
2023-02-14 11:41:41 +0700

    vue master basic

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <input placeholder="please input something" />
+  </header>
+
+  <main>
+    <HelloWorld msg="Hello" />
+  </main>
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<style scoped></style>
