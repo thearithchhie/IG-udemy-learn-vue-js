6f3106b16927899666d028c8658fae8d96c31fd7
thearithchhie <67414669+thearithchhie@users.noreply.github.com>
2023-02-18 07:21:23 +0700

    setup-parttern-composition

---
+++ src/views/HomeView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <div>
+      <button @click="decrementCounter" class="btn">-</button>
+      <span class="count">{{ counter }}</span>
+      <button @click="incrementCounter" class="btn">+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    const counter = ref(0);
+    const incrementCounter = () => {
+      counter.value++
+    }
+    const decrementCounter = () => {
+      if(counter.value > 0) {
+      counter.value--
+     }
+    }
+    return {
+      counter,
+      incrementCounter,
+      decrementCounter
+    }
+  }
+}
+</script>
+
+<style scope>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn, .count {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
